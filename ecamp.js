3a762bc71d76681c044f11b5457b63583115ce24
jeffersonadjistan <jefferson.adjistan@student.citrakasih.sch.id>
2025-10-13 20:11:45 +0700

    Add files via upload

---
+++ ecamp.js
@@ -0,0 +1,23 @@
+const moodLinks = {
+    Joy: "joy.html",
+    Proud: "proud.html",
+    Motivated: "motivated.html",
+    Amazed: "amazed.html",
+    Calm: "calm.html",
+    Okay: "okay.html",
+    Bored: "bored.html",
+    Lazy: "lazy.html",
+    Sad: "sad.html",
+    Guilty: "guilty.html",
+    Angry: "angry.html",
+    Disappointed: "disappointed.html"
+};
+
+Object.keys(moodLinks).forEach(mood => {
+    const btn = document.getElementById(mood);
+    if (btn) {
+        btn.addEventListener("click", () => {
+            window.location.href = moodLinks[mood];
+        });
+    }
+});
